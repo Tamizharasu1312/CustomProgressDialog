c654f777f6978594e342eb29bbe5a2bf9fcbb84e
Tamizharasu <tamizharasu@ngws.loacl>
2020-01-31 14:51:22 +0530

    CustomAVProgressBar

--- app/src/main/java/com/quebotic/customprogressdialog/activities/MainActivity.kt
+++ app/src/main/java/com/quebotic/customprogressdialog/activities/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.kaopiz.kprogresshud.KProgressHUD
 import com.quebotic.customprogressdialog.R
+import com.quebotic.customprogressdialog.utils.CustomAVProgressBar
 import com.quebotic.customprogressdialog.utils.CustomProgressBar
 import com.quebotic.customprogressdialog.utils.ProgressDialogs
 import com.thuongnh.zprogresshud.ZProgressHUD
@@ -23,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         CustomProgressBar()
     var progressHUD: ZProgressHUD? = null
     var mKProgressHUD: KProgressHUD? = null
-
+    val mPBAVProgress = CustomAVProgressBar()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,18 +39,28 @@ class MainActivity : AppCompatActivity() {
         mbtHide = findViewById(R.id.bt_hide)
 
         mbtShow!!.setOnClickListener {
-            /*progressHUD!!.setMessage("Please Wait...")
+
+            mPBAVProgress.show(
+                this,
+                "Please Wait..."
+            )
+
+            Handler().postDelayed({
+                mPBAVProgress.dismissWithSuccess("Success")
+                //  progressBar!!.dialog.dismiss()
+            }, 500)
+         /*   *//*progressHUD!!.setMessage("Please Wait...")
             progressHUD!!.setSpinnerType(2)
-            progressHUD!!.show()*/
+            progressHUD!!.show()*//*
 
             //ZProgressHUDBar(mContext!!).intProgressBar(1,"Please Wait...")
             // KProgressHUDBar(mContext!!).intProgressBar(1,"Please Wait...")
 
 
-            /*  mKProgressHUD!!.setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
+            *//*  mKProgressHUD!!.setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
                   .setDimAmount(0.5f)
                   .setLabel("Please Wait...")
-              mKProgressHUD!!.show()*/
+              mKProgressHUD!!.show()*//*
 
             //ProgressDialogFragment.newInstance("I AM TITLE!!").show(supportFragmentManager, "ProgressDialogs")
 
@@ -63,7 +74,7 @@ class MainActivity : AppCompatActivity() {
                 //KProgressHUDBar(mContext!!).scheduleDismiss()
                 //mKProgressHUD!!.dismiss()
             }, 3000)
-
+*/
         }
         mbtHide!!.setOnClickListener {
             Handler().postDelayed({
